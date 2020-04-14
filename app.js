212e17713262dccf7306451320e5b5f21889c697
Chris Cheng <chrisacheng@gmail.com>
2020-04-14 00:17:30 -0700

    API for getting scores.

--- app.js
+++ app.js
@@ -3,22 +3,38 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-let users = {}
 
+/* Kinda like Python's defaultdict, but for JS*/
+function defDict(type) {
+    const dict = {};
+    return {
+        get: function (key) {
+        if (!dict[key]) {
+            dict[key] = type.constructor();
+        }
+        return dict[key];
+        },
+        dict: dict
+    };
+}
+let rooms_to_scores = defDict({});
+
+app.use('/scores/:room', function(req, res) {
+  res.send(rooms_to_scores.get(req.params.room));
+});
 app.use(express.static('public'));
 
 io.on('connection', function(socket) {
   socket.on('enter', (room, user) => {
     socket.join(room);
+    rooms_to_scores.get(room)[user] = 0;
     io.emit('enter', user);
   });
   socket.on('chat message', (room, user, msg) => {
     io.to(room).emit('chat message', user, msg);
-    users[socket.id] = user;
   });
   socket.on('disconnect', function() {
-    io.emit('exit', users[socket.id]);
-    users[socket.id] = null;
+    // TODO: Broadcast disconnects.
   });
 });
 

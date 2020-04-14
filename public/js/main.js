c8bfdc9f44f57b326d5969073ee148f037dadac8
Jenny Cheng <31137328+jcheng1203@users.noreply.github.com>
2020-04-13 20:55:33 -0700

    Connect dialog to jquery. (#3)

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,18 @@
 const socket = io();
 let username = null;
 
-function enterChat(name) {
-  username = name
-  socket.emit('enter', name);
-}
-
 function appendMessage(msg) {
   $('#messages').append($('<li>').text(msg));
 }
 
 function main() {
-  $('form').submit(function(e) {
+  $('#name_form').submit(function(e) {
+    username = $('#name_input').val();
+    socket.emit('enter', username);
+    $('#name_form_dialog').hide();
+    return false;
+  });
+  $('#chat_form').submit(function(e) {
     e.preventDefault(); // prevents page reloading
     socket.emit('chat message', username, $('#m').val());
     $('#m').val('');

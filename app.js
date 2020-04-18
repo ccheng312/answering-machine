e91fd2beafb9291181c0fb3b8c70465c6e416205
Chris Cheng <chrisacheng@gmail.com>
2020-04-18 00:39:43 -0700

    Switch to pug templates.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const rooms_lib = require('./lib/rooms');
 
+app.set('view engine', 'pug');
 
 // Routes
 app.post('/rooms', function(req, res) {
